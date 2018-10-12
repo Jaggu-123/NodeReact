3f08f7b1ac6531e95aa8ad471a337a4d5f10f4d1
Jaggu <khushboojagnani254@gmail.com>
2018-10-13 01:37:39 +0530

    added materialize and designed Header

---
+++ server/client/src/components/Header.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+    render() {
+        return (
+            <nav>
+                <div className="nav-wrapper indigo darken-3">
+                    <a href="#" className="left brand-logo">
+                        Emaily
+                    </a>
+
+                    <ul className="right">
+                        <li>
+                            <a href="#">Login With Google</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
+}
+
+export default Header;
