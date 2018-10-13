dccfce4c887204f04f2fe574c297afe7b8e95a6f
Jaggu <khushboojagnani254@gmail.com>
2018-10-13 13:48:56 +0530

    converted async syntax

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -3,10 +3,7 @@ import axios from "axios";
 import { FETCH_USER } from "./types";
 
 //ajax request to backend server for getting current_user
-export const fetchUser = () => {
-    return function(dispatch) {
-        axios
-            .get("/api/current_user")
-            .then(res => dispatch({ type: FETCH_USER, payload: res }));
-    };
+export const fetchUser = () => async dispatch => {
+    const res = await axios.get("/api/current_user");
+    dispatch({ type: FETCH_USER, payload: res });
 };
