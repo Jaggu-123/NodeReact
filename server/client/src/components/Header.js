099dc0c80b9723390e4d4ac1c401a5a680c8736c
Jaggu <khushboojagnani254@gmail.com>
2018-10-13 20:40:45 +0530

    Created Landing and connect Link to Header-brand

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 class Header extends Component {
     renderContent() {
@@ -25,9 +26,12 @@ class Header extends Component {
         return (
             <nav>
                 <div className="nav-wrapper indigo darken-3">
-                    <a href="#" className="left brand-logo">
+                    <Link
+                        to={this.props.auth ? "/surveys" : "/"}
+                        className="left brand-logo"
+                    >
                         Emaily
-                    </a>
+                    </Link>
 
                     <ul className="right">{this.renderContent()}</ul>
                 </div>
---
+++ server/client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Landing = () => {
+    return (
+        <div style={{ textAlign: "center" }}>
+            <h1>Emaily!</h1>
+            <p>Collect Feedback from your Users</p>
+        </div>
+    );
+};
+
+export default Landing;
