0b1b1ed9af50e3ca14c6bfa30286899f69c7d23a
Jaggu <khushboojagnani254@gmail.com>
2019-01-27 10:10:58 +0530

    setup for production

--- server/index.js
+++ server/index.js
@@ -28,5 +28,18 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app); //will turn into a function with app as an argument
 
+if (process.env.NODE_ENV === "production") {
+    //Express will serve up production assets
+    //like our main.js or main.css file
+    app.use(express.static("client/build"));
+
+    //Express will serve up the index.html file
+    //if it does not recognize the route
+    const path = require("path");
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
